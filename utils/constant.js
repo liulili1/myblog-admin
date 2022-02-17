a60158809949c25f4f9fec84be6319ac26967799
liulili <731099344@qq.com>
2022-02-17 19:44:10 +0800

    更改数据库配置

--- utils/constant.js
+++ utils/constant.js
@@ -1,4 +1,4 @@
-const {env} = require('./env')
+const { env } = require('./env')
 // const UPLOAD_PATH = env === 'dev' ? '/Users/liulili/upload/myblog' : '/root/upload/myblog'
 // const UPLOAD_URL = env === 'dev' ? 'http://localhost:8089/myblog/blog': 'http://www.sosoleona.xyz/book/res/img'
 module.exports = {
@@ -6,9 +6,9 @@ module.exports = {
   ERROR_CODE: -1,
   CODE_TOKEN_EXPIRED: -2,
   PWS_SALL: 'liulili_blog_admin',
-  debug: true,
+  debug: false,
   PRIVATE_KEY: 'liulili_blog_admin_key',
   JWT_EXPIRES: 60 * 60,
-  UPLOAD_PATH: '/Users/liulili/upload/myblog',
-  UPLOAD_URL: 'http://localhost:8089/myblog'
+  UPLOAD_PATH: env==='env'?'/Users/liulili/upload/myblog':'/root/upload/myblog',
+  UPLOAD_URL: env==='env'?'http://localhost:8089/myblog':'http://www.sosoleona.xyz/book/res/img'
 }
